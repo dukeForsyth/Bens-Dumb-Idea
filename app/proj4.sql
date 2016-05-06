23c32c0ea56a4098f496b6d072b2687307082b34
Ben <blw31415@vt.edu>
2016-05-06 14:25:35 -0400

    updated server info

--- app/proj4.sql
+++ app/proj4.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 08, 2016 at 06:56 PM
+-- Generation Time: May 06, 2016 at 08:24 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -45,7 +45,12 @@ INSERT INTO `activities` (`userID`, `dateMade`, `content`, `unique_id`, `recieve
 (1, '0000-00-00 00:00:00', 'admin followed c!', 15, 3, 'followed', 0),
 (1, '0000-00-00 00:00:00', 'admin followed c!', 16, 3, 'followed', 0),
 (3, '0000-00-00 00:00:00', '', 17, 0, 'publish', 4),
-(1, '0000-00-00 00:00:00', 'admin has liked c''s build 4', 18, 3, 'liked', 4);
+(1, '0000-00-00 00:00:00', 'admin has liked c''s build 4', 18, 3, 'liked', 4),
+(1, '0000-00-00 00:00:00', 'admin has added part AMD FD6300WMHKBOX FX-6300 to build 1', 19, 0, 'edited', 1),
+(1, '0000-00-00 00:00:00', 'admin has added part Gigabyte AM3+ AMD DDR3 to build 1', 20, 0, 'edited', 1),
+(1, '0000-00-00 00:00:00', 'admin has added part AMD FD6300WMHKBOX FX-6300 to build 1', 21, 0, 'edited', 1),
+(1, '0000-00-00 00:00:00', 'admin has added part AMD FD6300WMHKBOX FX-6300 to build 1', 22, 0, 'edited', 1),
+(1, '0000-00-00 00:00:00', 'admin has added part Gigabyte AM3+ AMD DDR3 to build 1', 23, 0, 'edited', 1);
 
 -- --------------------------------------------------------
 
@@ -68,7 +73,7 @@ CREATE TABLE `builds` (
 --
 
 INSERT INTO `builds` (`unique_id`, `userkey`, `cpu_id`, `videocard_id`, `motherboard_id`, `memory_id`, `storage_id`) VALUES
-(1, 1, 'B009O7YORK', '', '', '', ''),
+(1, 1, 'B009O7YU56', 'B00847TPH0', '', 'B006YG8X9Y', 'B00W8XXRPM'),
 (2, 1, '', '', '', '', ''),
 (4, 3, '', '', '', '', '');
 
@@ -101,7 +106,7 @@ INSERT INTO `follower` (`userID`, `followingID`, `unique_id`) VALUES
 CREATE TABLE `parts` (
   `unique_id` varchar(25) DEFAULT NULL,
   `part_type` varchar(25) NOT NULL,
-  `name` varchar(25) NOT NULL
+  `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -111,15 +116,24 @@ CREATE TABLE `parts` (
 INSERT INTO `parts` (`unique_id`, `part_type`, `name`) VALUES
 ('B009O7YORK', 'cpu', 'AMD FD6300WMHKBOX FX-6300'),
 ('B009O7YU56', 'cpu', 'AMD FD8320FRHKBOX FX-8320'),
-('B00847TPH0', 'videocard', 'EVGA GeForce GT 610'),
 ('B009FC3YJ8', 'motherboard', 'Gigabyte AM3+ AMD DDR3'),
-('B006WAGGUK', 'memory', 'Crucial Ballistix Sport'),
 ('B00J8E8Y5C', 'memory', 'Kingston HyperX FURY 16GB'),
 ('B006YG8X9Y', 'memory', 'Crucial 8GB Single DDR3'),
-('B00H4XH5GI', 'storage', 'Seagate Backup Plus Slim '),
-('B00TKFEEAS', 'memory', 'Seagate Expansion 1TB'),
-('B00W8XXRPM', 'storage', 'WD 1TB Black'),
-('B00UVN21RQ', 'videocard', 'Nvidia Titan X');
+('B00UVN21RQ', 'videocard', 'Nvidia Titan X'),
+('B00JST2QEW', 'cpu', 'i5 4590'),
+('B00K23BW70', 'motherboard', 'MSI Intel Z97 LGA 1150 DDR3 USB 3.1 ATX Motherboard '),
+('B00NT9UT3M', 'videocard', 'EVGA GeForce GTX 980'),
+('B00FLMKQYW', 'videocard', 'Sapphire Radeon R9 290 4GB GDDR5'),
+('B012M8LXQW', 'cpu', 'i7 6700k'),
+('B0088PUEPK', 'storage', 'Western Digital Caviar Blue 1TB 3.5" 7200RPM Internal Hard Drive'),
+('B00OAJ412U', 'storage', 'Samsung 850 EVO-Series 250GB 2.5" Solid State Drive'),
+('B005T3GRN2', 'storage', 'Seagate Barracuda 2TB 3.5" 7200RPM Internal Hard Drive'),
+('B005T3GRNW', 'storage', 'Seagate Barracuda 1TB 3.5" 7200RPM Internal Hard Drive'),
+('B00NVODXR4', 'videocard', 'EVGA GeForce GTX 970 4GB'),
+('B0131GA4WI', 'motherboard', 'MSI Z170A GAMING M5 ATX LGA1151 Motherboard'),
+('B00TOBQQTK', 'motherboard', 'ASRock 970M PRO3 Micro ATX AM3+/AM3 Motherboard'),
+('B0134EW7G8', 'memory', 'Corsair Vengeance LPX 16GB (2 x 8GB) DDR4-3000 Memory'),
+('B00J8E9334', 'memory', 'Kingston HyperX Fury Black 8GB (1 x 8GB) DDR3-1600 Memory');
 
 -- --------------------------------------------------------
 
@@ -182,7 +196,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `activities`
 --
 ALTER TABLE `activities`
-  MODIFY `unique_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `unique_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `builds`
 --
